874be3542795e542bac38cc327e737a3a481c78e
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:31:15 +0300

    MassTransitSetup.cs

---
+++ Financial-Chat/Services/MassTransitSetup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using MassTransit;
+using GreenPipes;
+using Financial.Chat.Domain.Shared.Entity;
+
+namespace Financial.Chat.Web.API.Config
+{
+    public static class MassTransitSetup
+    {
+        public static void AddMassTransitSetup(this IServiceCollection services, RabbitMqOptions options)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host(new Uri(options.Url), h =>
+                {
+                    h.Username(options.Username);
+                    h.Password(options.Password);
+                });
+            });
+
+            services.AddSingleton(busControl);
+        }
+    }
+}
