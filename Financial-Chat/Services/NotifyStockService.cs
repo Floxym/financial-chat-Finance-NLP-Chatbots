01a2857470e7e569aefab82242caf4636c1c746f
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:31:24 +0300

    NotifyStockService.cs

---
+++ Financial-Chat/Services/NotifyStockService.cs
@@ -0,0 +1,35 @@
+﻿using Chat.Domain.Hubs;
+using Chat.Domain.Interfaces;
+using Chat.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Chat.Domain.Services
+{
+    public class NotifyStockService : INotifyStockService
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public NotifyStockService(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task NotifyRoomUserAsync(string code, string userId, string roomId, string value)
+        {
+            var date = (int)DateTimeOffset.Now.ToUnixTimeSeconds();
+            string message;
+            if (code.Contains("Error"))
+            {
+                message = code;
+            }
+            else
+            {
+                message = string.Concat(code, " quote is ", value, " per shre");
+            }
+
+            await _hubContext.Clients.Group(roomId).SendAsync("ReceiveMessage", userId, message, date, "all");
+        }
+    }
+}
