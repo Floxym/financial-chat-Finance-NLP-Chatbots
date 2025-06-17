1e16b7a51139d39971f11b113d2c83dc03e8ef4e
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:30:14 +0300

    FinancialChatContext.cs

---
+++ Financial-Chat/infra.external/FinancialChatContext.cs
@@ -0,0 +1,37 @@
+﻿using Financial.Chat.Domain.Core.Entity;
+using Financial.Chat.Infra.Data.Mapping;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Financial.Chat.Infra.Data.Context
+{
+    public class FinancialChatContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Messages> Messages { get; set; }
+        public FinancialChatContext(DbContextOptions<FinancialChatContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                 .AddJsonFile($"appsettings.Development.json")
+#else
+                 .AddJsonFile($"appsettings.Production.json")
+#endif
+                 .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("FinancialChatConnection"));
+        }
+    }
+}
